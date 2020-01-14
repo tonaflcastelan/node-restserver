c76777d144ae8b129754a2dc9f55d68398f4487b
tonacastelan <tonatiuh.flores@konfio.mx>
2020-01-14 17:37:48 -0600

    variable de enterono mongo

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://node-mongo-db:2apuW5yXJidrXQkL@cluster0-nq3je.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URL_DB = urlDB;
